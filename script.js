542e4626e7de595da13d84a6a6678948c613acdc
Rayngumi <rayngumi25@gmail.com>
2024-04-28 00:35:39 +0300

    added html,css,js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('getJokeBtn').addEventListener('click', getChuckNorrisJoke);
+
+function getChuckNorrisJoke() {
+  fetch('https://api.chucknorris.io/jokes/random')
+    .then(response => response.json())
+    .then(data => {
+      const jokeElement = document.createElement('p');
+      jokeElement.textContent = data.value;
+      document.getElementById('jokeContainer').appendChild(jokeElement);
+    })
+    .catch(error => {
+      console.error('Error fetching Chuck Norris joke:', error);
+    });
+}
